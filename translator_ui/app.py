a7936990233037181f75dc4e04757c9c2d41f3ce
md-gpu <eggs@countingchickens.co.uk>
2021-09-09 10:22:35 +0100

    two-way translate & playback speed

--- translator_ui/app.py
+++ translator_ui/app.py
@@ -22,30 +22,50 @@ def translate_from_english():
         'show_results':False
     }
     qry = request.form.get("query")
+    lang_in = request.form.get("language")
+    if lang_in == 'en':
+        source_language = 'en'
+        target_language = 'bg'
+    elif lang_in == 'bg':
+        source_language = 'bg'
+        target_language = 'en'
+        
+    print(lang_in)
     data = {
         "user_id": "user1234",
         "text": str(qry),
-        "source_language": "en",
-        "target_language": "bg",
+        "source_language": source_language,
+        "target_language": target_language,
         "path_to_gcp_creds": "../gcp_creds.json"
     }
 
     result_translate = call_fast_api(data, endpoint = 'translate_gcp/', port=video_editor_port, action='POST', host = video_editor_host)
     
+    if lang_in == 'en':
+        image_search_qry = str(qry)
+        voice_qry_text = result_translate['translated_text']
+        voice_qry_voice = "bg-bg-Standard-A"
+        reverse_translit = False
+    elif lang_in == 'bg':
+        image_search_qry = str(result_translate['translated_text'])
+        voice_qry_text = result_translate['translated_text']
+        voice_qry_voice = "en-GB-Wavenet-D"
+        reverse_translit = True
+
     data = {
         "user_id": "user1234",
-        "search_query": str(qry),
+        "search_query": image_search_qry,
         "search_domains": [
-            "Google"
+            "Google_image"
         ],
-        "response_size_each_domain": 3
+        "response_size_each_domain": 5
     }
     result_search = call_fast_api(data, endpoint = 'search_request/', port=search_service_port, action='POST', host = search_service_host)
            
     data = {
         "user_id": "user1234",
-        "text": result_translate['translated_text'],
-        "voice_name": "bg-bg-Standard-A",
+        "text": voice_qry_text,
+        "voice_name": voice_qry_voice,
         "output_path": "../data/speaker",
         "path_to_gcp_creds": "../gcp_creds.json"
     }
@@ -55,9 +75,9 @@ def translate_from_english():
         #page_config = result_tts
         page_config['text'] = result_translate['text']
         page_config['translated_text'] = result_translate['translated_text']
-        page_config['translit_text'] = transliterate(result_translate['translated_text'])
+        page_config['translit_text'] = transliterate(result_translate['translated_text'], reverse=reverse_translit)
         page_config['file_name'] = result_tts['file_name']
-        result_search['images'] = [r["thumbnail"] for r in result_search['search_response']]
+        page_config['images'] = [r["thumbnail"] for r in result_search['search_response']]
 
         page_config['show_results'] = True
     return render_template('home.html',page_config = page_config)
